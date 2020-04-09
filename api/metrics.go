614f94332fc9fdc81d789ba126171ce33f3f1e9c
Cedric Lewe <43417046+QuByte-Zer0@users.noreply.github.com>
2020-04-09 02:22:07 +0200

    Metrics: add path label to qumine_ingress_api_time

--- api/metrics.go
+++ api/metrics.go
@@ -2,9 +2,9 @@ package api
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promauto"
 )
 
 var (
@@ -15,23 +15,27 @@ var (
 		},
 		[]string{"path"},
 	)
-	metricsAPITime = promauto.NewHistogram(prometheus.HistogramOpts{
-		Name:    "qumine_ingress_api_time",
-		Help:    "The response time of the api",
-		Buckets: prometheus.LinearBuckets(0.01, 0.01, 10),
-	})
+	metricsAPITime = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "qumine_ingress_api_time",
+			Help:    "The response time of the api",
+			Buckets: prometheus.LinearBuckets(0.01, 0.01, 10),
+		},
+		[]string{"path"},
+	)
 )
 
 func init() {
 	prometheus.MustRegister(metricsAPIRequestsTotal)
+	prometheus.MustRegister(metricsAPITime)
 }
 
 func metricsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
-		timer := prometheus.NewTimer(metricsAPITime)
-		defer timer.ObserveDuration()
-
 		metricsAPIRequestsTotal.With(prometheus.Labels{"path": request.URL.Path}).Inc()
+
+		start := time.Now()
 		next.ServeHTTP(writer, request)
+		metricsAPITime.With(prometheus.Labels{"path": request.URL.Path}).Observe(float64(time.Since(start).Milliseconds()))
 	})
 }
