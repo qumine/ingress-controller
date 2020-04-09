6d72ead98d979c3dab598aaa4598909b1ad7f618
Cedric Lewe <43417046+QuByte-Zer0@users.noreply.github.com>
2020-04-09 02:13:52 +0200

    Metrics: add path label to qumine_ingress_api_requests

--- api/metrics.go
+++ api/metrics.go
@@ -8,10 +8,13 @@ import (
 )
 
 var (
-	metricsAPIRequestsTotal = promauto.NewCounter(prometheus.CounterOpts{
-		Name: "qumine_ingress_api_requests_total",
-		Help: "The total number of api requests",
-	})
+	metricsAPIRequestsTotal = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "qumine_ingress_api_requests_total",
+			Help: "The total number of api requests",
+		},
+		[]string{"path"},
+	)
 	metricsAPITime = promauto.NewHistogram(prometheus.HistogramOpts{
 		Name:    "qumine_ingress_api_time",
 		Help:    "The response time of the api",
@@ -19,12 +22,16 @@ var (
 	})
 )
 
+func init() {
+	prometheus.MustRegister(metricsAPIRequestsTotal)
+}
+
 func metricsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
 		timer := prometheus.NewTimer(metricsAPITime)
 		defer timer.ObserveDuration()
 
-		metricsAPIRequestsTotal.Inc()
+		metricsAPIRequestsTotal.With(prometheus.Labels{"path": request.URL.Path}).Inc()
 		next.ServeHTTP(writer, request)
 	})
 }
