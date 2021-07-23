6e4cd2d24c156e5f1caaf1905d339d31ee9da29c
Cedric Lewe <43417046+0SkillAllLuck@users.noreply.github.com>
2021-07-23 23:29:46 +0200

    feat: simplify LivenessProbe

--- internal/api/health.go
+++ internal/api/health.go
@@ -3,17 +3,8 @@ package api
 import "net/http"
 
 func (api *API) healthLive(writer http.ResponseWriter, request *http.Request) {
-	details := make(map[string]string)
-	details["k8s"] = api.k8s.Status
-	details["server"] = api.ing.Status
-
-	if api.k8s.Status == "up" && api.ing.Status == "up" {
-		writer.WriteHeader(http.StatusOK)
-		writer.Write([]byte{})
-	} else {
-		writer.WriteHeader(http.StatusServiceUnavailable)
-		writer.Write([]byte{})
-	}
+	writer.WriteHeader(http.StatusOK)
+	writer.Write([]byte{})
 }
 
 func (api *API) healthReady(writer http.ResponseWriter, request *http.Request) {
